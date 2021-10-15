c5da451390c078049f14e1af548200b4634fff81
xinsheng-G <hayama611@gmail.com>
2021-10-16 09:32:22 +1100

    Add CSS code for the weather panel

--- script.js
+++ script.js
@@ -101,6 +101,7 @@ map.on('load', e => {
     })
 });
 
+// Javascript code for ther filter menu stars from here //
 map.on('idle', () => {
     if (!map.getLayer('Melbourne_Municipal_Boundary') || !map.getLayer('Melbourne_CityCircle_tram')
     || !map.getLayer('BusMetroRoutes') || !map.getLayer('Melbourne_Street_Names') 
@@ -152,3 +153,92 @@ map.on('idle', () => {
     }
 });
   
+
+//// Javascript code for the weather panel stars from here ////
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+// Set the weather panel data
+const weather = {};
+weather.temperature = {
+    unit : "celsius"
+}
+
+// OpenWeather key
+const K = 273;
+const key = "46056ec3e71acc6cec6e3cae8884eb22";
+
+// Check user's geolocation information, and whether the browswer support the geolocation function
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Geolocation not enabled</p>";
+}
+
+// Locate the current location of user
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    
+    getWeather(latitude, longitude);
+}
+
+// Show error message when geolocation function cannot work
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+// Fetch weather condition data from API
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - K);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+
+// Display data on the interface
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// Conversion from Celsius to Fahrenheit 
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+// On click effect from user
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
