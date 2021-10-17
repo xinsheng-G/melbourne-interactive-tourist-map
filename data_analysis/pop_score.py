fd0d0543f01ce858e6a667578cf297ee2f6b16fd
yjc95 <yujiacheng10@gmail.com>
2021-10-17 23:52:38 +1100

    popular scores

--- data_analysis/pop_score.py
+++ data_analysis/pop_score.py
@@ -1,2 +1,13 @@
 import pandas as pd
 
+ped_2019 = pd.read_csv('../data/pedestrain_2019.csv')
+
+ped_2019 = ped_2019[['Sensor_ID','Month','Hourly_Counts']]
+
+print(ped_2019["Sensor_ID"])
+
+ped_34 = ped_2019[(ped_2019["Sensor_ID"] == 34)]
+
+ped_Month = ped_34[(ped_34['Month'] == 'November')]
+
+print(ped_Month)
